a8dfb6e12868de11d990024774fe708f4beda3d0
kdd9512 <hyunwoo8805@gmail.com>
2022-02-05 22:23:59 +0900

    20220205 1st commit
    초기 설정.

---
+++ src/main/java/com/sbmoviev2/Sbmoviev2Application.java
@@ -0,0 +1,13 @@
+package com.sbmoviev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbmoviev2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbmoviev2Application.class, args);
+    }
+
+}
