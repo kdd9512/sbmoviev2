e2e17955a0d416b42483c323dc9144bc0dbbc0da
kdd9512 <hyunwoo8805@gmail.com>
2022-02-08 23:41:35 +0900

    20220208 3rd commit.
    양방향참조 예제.
    
    mappedBy 이용하여 각 Entity 간 상하관계 지정.
    Entity 에 자료 삭제 test.
    cascade 이용하여 Entity 에 새 movie title 생성과 자료 삽입 동시 처리.
    orphanRemoval 이용하여 참조가 없는 하위 Entity 객체 관리(삭제 or 유지).

--- src/main/java/com/sbmoviev2/entity/Movie.java
+++ src/main/java/com/sbmoviev2/entity/Movie.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
 @Builder
@@ -25,13 +26,36 @@ public class Movie extends BaseEntity{
     // 주체는 movie 이다.
     @Builder.Default
     // cascade 속성을 지정하여 movie 속성과 이하에서 저장하는 addPoster 의 poster 의 데이터를 함께 삽입한다.
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "movie", cascade = CascadeType.ALL)
+    // orphanRemoval 속성 : 참조가 없는 하위 Entity 객체는 삭제할 것인가? T/F
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "movie", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Poster> posterList = new ArrayList<>();
 
     public void addPoster(Poster poster){
         poster.setIdx(this.posterList.size());
         poster.setMovie(this);
         posterList.add(poster);
+    }
+
+
+    public void removePoster(Long ino) {
+
+        Optional<Poster> result = posterList.stream().filter(p -> p.getIno() == ino).findFirst();
+
+        result.ifPresent(p -> {
+            p.setMovie(null); // 하위 Entity 삭제.
+            posterList.remove(p); // 해당 번호의 Poster 찾아서 삭제.
+        });
+
+        // Poster 중 하나를 삭제했으므로, 기존 Poster 객체의 idx 값을 변경.
+        changeIdx();
+
+    }
+
+    private void changeIdx(){
+        for (int i = 0; i < posterList.size(); i++) {
+            posterList.get(i).setIdx(i);;
+        }
+
     }
 
 }
--- src/test/java/com/sbmoviev2/repository/MovieRepositoryTests.java
+++ src/test/java/com/sbmoviev2/repository/MovieRepositoryTests.java
@@ -6,6 +6,8 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Log4j2
@@ -27,6 +29,32 @@ public class MovieRepositoryTests {
 
         log.info(movie.getMno());
 
+    }
+
+    @Test
+    @Transactional // LazyInitialization 에러 방지를 위한 annotation. 지연조회 시점까지 세션을 유지시킨다.
+    @Commit
+    public void testAddPoster(){
+
+        // getOne 은 deprecated 되었음. 때문에 지연조회까지 세션유지를 위한 @Transactional annotation 이 추가로 필요하다.
+        Movie movie = repository.getById(1L); // DB 상 존재하는 영화번호.
+
+        movie.addPoster(Poster.builder().fname("file3.jpg").build()); // 새 poster 객체.
+
+        repository.save(movie);
+
+    }
+
+    @Test
+    @Transactional
+    @Commit
+    public void testRemovePoster(){
+
+        Movie movie = repository.getById(1L);
+
+        movie.removePoster(2L);
+
+        repository.save(movie);
     }
 
 }
